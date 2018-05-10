1dd7d5ababe63408a1c9a6705343421a3be35e07
Alex <alexofrontend@gmail.com>
2018-05-10 00:26:33 -0400

    added gulp

---
+++ client/gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('default', ['css'], () => {
+  gulp.watch("./src/sass/**.scss", ['css'])
+})
+
+gulp.task('css', () => {
+  gulp.src("src/sass/*.scss")
+  .pipe(sass({outputStyle: 'compressed'})
+  .on('error', sass.logError) 
+  .pipe(autoprefixer({browsers: ['last 2 versions']}))
+  .pipe(gulp.dest("public/css"));
+})
