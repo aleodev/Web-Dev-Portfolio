fe84b35b1c893cf1a6a58d9e16d99c84dd266d63
Alex <alexofrontend@gmail.com>
2018-05-10 00:23:56 -0400

    fixed packages and removed default favicon

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -19,7 +18,7 @@ app.use('/', index);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
+// Logger
 app.use(logger('dev'));
 
 // Body Parsing
