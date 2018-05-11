1e06edb99e0b79207236d6f3786e2cb857e371b4
Alex <alexofrontend@gmail.com>
2018-05-11 17:30:45 -0400

    added 404 handler and site design update

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var app = express();
 
 // Routes
 app.use('/', index);
+app.use(express.static(path.join(__dirname, '../build')));
 
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -32,27 +33,34 @@ app.use(cookieParser());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(compression());
 
+// Standard 404
 
-// 404
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // 404
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // Error Handler
+// app.use(function(err, req, res, next) {
+//   // Only Err In Dev
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development'
+//     ? err
+//     : {};
+//
+//   // Render an error
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-// Error Handler
-app.use(function(err, req, res, next) {
-  // Only Err In Dev
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development'
-    ? err
-    : {};
+//Portfolio 404(Redirect to homepage)
+app.use(function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
-  // Render an error
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-var server = app.listen( process.env.PORT || 3001, function(){
+//Server Initialize
+var server = app.listen(process.env.PORT, function(){
   console.log('Listening on port ' + server.address().port);
 });
