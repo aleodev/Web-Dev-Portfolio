3d527090e20ed97519abcd733814a6e882e4bf0b
Alex <alexofrontend@gmail.com>
2018-05-11 08:22:53 -0400

    routed / to index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 var request = require('request')
 var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Alex O Resume'})
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
 module.exports = router;
