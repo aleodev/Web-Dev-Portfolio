e2544beedde5c2bacea2a0afa4831b2939c074a0
Alex <alexofrontend@gmail.com>
2018-05-15 12:38:48 -0400

    Added 4 main pages

---
+++ client/src/js/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = (props) => {
+  return (
+    <section id="about"></section>
+  )
+}
+
+export default About;
---
+++ client/src/js/components/Works.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Works = (props) => {
+  return (
+    <section id="works"></section>
+  )
+}
+
+export default Works;
