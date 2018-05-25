6efe22612945dcf052c8b6d18ee13a89d2177778
Alex <alexofrontend@gmail.com>
2018-05-24 23:49:40 -0400

    final header fix

--- client/src/js/components/Home.js
+++ client/src/js/components/Home.js
@@ -4,11 +4,6 @@ const Home = (props) => {
   return (<section className="section-page" id="home-page">
     <div className="home-container">
       <div className="name">Alex Ortiz</div>
-      <div className="home-social">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/aleodev" id="github"><img id="socialmedia" src="/assets/img/social/github.png.gz"/></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Alexodev_" id="twitter"><img id="socialmedia" src="/assets/img/social/twitter.png.gz"/></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://instagram.com/" id="instagram"><img id="socialmedia" src="/assets/img/social/instagram.png.gz"/></a>
-      </div>
       <div className="text">I’m a<span>
           frontend developer</span>,
         <span>photographer</span>, and a
