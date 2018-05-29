d16afbda62db3b32c9b0193472a1f52d71813fdc
Alex <alexofrontend@gmail.com>
2018-05-29 00:26:48 -0400

    turned home into a class

--- client/src/js/components/Home.js
+++ client/src/js/components/Home.js
@@ -1,13 +1,40 @@
-import React from 'react'
-
-const Home = (props) => {
-  return (<section className="section-page" id="home-page">
-    <div className="home-container">
-      <div className="name">Alex Ortiz</div>
-      <div className="text">I’m a <span>frontend developer</span>, <span>photographer</span>, and a<span> passionate learner</span> that strives to maximize their capacity and capability of productive and functional code.</div>
-      <a href='#about-page'><span></span>Scroll</a>
-    </div>
-  </section>)
+import React, {Component} from 'react'
+import sr from '../pckgs/scrollReveal.js'
+class Home extends Component {
+  componentDidMount(){
+    sr.reveal('.name', {
+      origin: 'top',
+      duration: 1000,
+      delay: 150,
+      distance: '200px',
+      scale: 1,
+      easing: 'ease',
+      opacity: 0
+    })
+    sr.reveal('.text', {
+      origin: 'bottom',
+      duration: 1000,
+      delay: 150,
+      distance: '200px',
+      scale: 1,
+      easing: 'ease',
+      opacity: 0
+    })
+  }
+  render () {
+    return (<section className="section-page" id="home-page">
+      <div className="home-container">
+        <div className="name">Alex Ortiz</div>
+        <div className="text">I’m a
+          <span>frontend developer</span>,
+          <span>photographer</span>, and a<span>
+            passionate learner</span>
+          that strives to maximize their capacity and capability of productive and functional code.</div>
+        <a href='#about-page'>
+          <span></span>Scroll</a>
+      </div>
+    </section>)
+  }
 }
 
 export default Home
