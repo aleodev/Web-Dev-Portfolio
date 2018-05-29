27d3deeaf50ee18b75f10f997ea010607e117b5b
Alex <alexofrontend@gmail.com>
2018-05-28 23:46:59 -0400

    About section grid items

--- client/src/js/components/About.js
+++ client/src/js/components/About.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const About = (props) => {
-  return (
-    <section className="section-page" id="about-page"></section>
-  )
+  return (<section className="section-page" id="about-page">
+    <div className="about-side item">
+      <h1>About Me</h1>
+      <hr/>
+      <h2>I'm a front end developer based in the heart of <span>New York City</span>.</h2>
+      <p>I'm in love with the concept of thoughts being turned into a reality. Whether it'd be a website design, or a photo, turning these storms of thought into a functional form makes me smile like nothing else. </p>
+  </div>
+    <div className="service-side item">
+      <h1>Services</h1>
+            <hr/>
+    </div>
+    <div className="skills-side item">
+      <h1>Skills</h1>
+            <hr/>
+    </div>
+
+  </section>)
 }
 
 export default About
