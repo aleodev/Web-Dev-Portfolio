47edabd2fc3775a9d75bdb7305ceb9442ab09eff
Alex <alexofrontend@gmail.com>
2018-06-03 23:54:12 -0400

    home spacing fix

--- client/src/js/components/Home.js
+++ client/src/js/components/Home.js
@@ -25,7 +25,7 @@ class Home extends Component {
     return (<section className="section-page" id="home-page">
       <div className="home-container">
         <div className="name">Alex Ortiz</div>
-        <div className="text">I’m a<span>frontend developer</span>, <span>photographer</span>, and a<span> passionate learner</span> that strives to maximize their capacity and capability of productive and functional code.</div>
+        <div className="text">I’m a<span> frontend developer </span>,<span> photographer </span>, and a <span> passionate learner </span>that strives to maximize their capacity and capability of productive and functional code.</div>
         <a href='#about-page'>
           <span></span>Scroll</a>
       </div>
