6eb96e0894ab276ee29f6b72551fd4412379d58c
Alex <alexofrontend@gmail.com>
2018-06-30 23:37:43 -0400

    added sr reveal for portrait

--- client/src/js/components/Home.js
+++ client/src/js/components/Home.js
@@ -1,7 +1,16 @@
 import React, {Component} from 'react'
 import sr from '../pckgs/scrollReveal.js'
 class Home extends Component {
-  componentDidMount(){
+  componentDidMount () {
+    sr.reveal('.portrait', {
+      origin: 'top',
+      duration: 1250,
+      delay: 250,
+      distance: '200px',
+      scale: 1,
+      easing: 'ease',
+      opacity: 0
+    })
     sr.reveal('.name', {
       origin: 'top',
       duration: 1000,
@@ -24,10 +33,12 @@ class Home extends Component {
   render () {
     return (<section className="section-page" id="home-page">
       <div className="home-container">
+        <div className="portrait"><img src="/assets/img/portrait.jpg.gz" alt="Highschool Prom"/></div>
         <div className="name">Alex Ortiz</div>
-        <div className="text">I’m a<span> frontend developer </span>,<span> photographer </span>, and a <span> passionate learner </span>that strives to maximize their capacity and capability of productive and functional code.</div>
-        <a href='#about-page'>
-          <span></span>Scroll</a>
+        <div className="text">I’m a <span>frontend developer</span>, <span>photographer</span>, and a <span>passionate learner</span> that strives to maximize their capacity and capability of productive and functional code.</div>
+        <div className="pageSwitcher">
+          <a href='#about-page'><div className="switcher-particle"><span></span></div></a>
+        </div>
       </div>
     </section>)
   }
