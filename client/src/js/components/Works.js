b2c112914fa8b80da791ca3e13dce925659ff2bd
Alex <alexofrontend@gmail.com>
2018-07-07 16:29:27 -0400

    added blocks for the projects

--- client/src/js/components/Works.js
+++ client/src/js/components/Works.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Works = (props) => {
-  return (
-    <section id="works-page"></section>
-  )
+  return (<section id="works-page">
+  <div className="block work-1"></div>
+  <div className="block work-2"></div>
+  <div className="block work-3"></div>
+  <div className="block work-4"></div>
+    </section>)
 }
 
 export default Works
