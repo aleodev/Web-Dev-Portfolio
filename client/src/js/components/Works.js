a5ffe9bfd86017280b95a112d924ba6361b2b1cf
Alex <alexofrontend@gmail.com>
2018-07-10 23:33:23 -0400

    eslint fixes

--- client/src/js/components/Works.js
+++ client/src/js/components/Works.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Works = (props) => {
   return (<section id="works-page">
-  <div className="block work-1"></div>
-  <div className="block work-2"></div>
-  <div className="block work-3"></div>
-  <div className="block work-4"></div>
-    </section>)
+    <div className="block work-1"></div>
+    <div className="block work-2"></div>
+    <div className="block work-3"></div>
+    <div className="block work-4"></div>
+  </section>)
 }
 
 export default Works
